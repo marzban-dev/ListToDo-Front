319edaaad0cfe8d6869fec50ad2df33934dfa87f
X-Dark-Coder <marzban.dev@gmail.com>
2022-03-01 15:05:45 +0330

    Update projects page

--- src/pages/Projects/index.jsx
+++ src/pages/Projects/index.jsx
@@ -1,45 +1,72 @@
-import React, {useState} from "react";
-import {arrayMoveImmutable} from "array-move";
-import {setProjects, updateProjectPosition} from "store/actions/Projects.actions";
+import React from "react";
 import FloatButton from "components/UI/FloatButton";
 import PageContainer from "components/UI/PageContainer";
 import {useQueryClient} from "react-query";
-import {useDispatch} from "react-redux";
 import ShowProjects from "components/ShowProjects";
 import LoadingWrapper from "components/UI/LoadingWrapper";
+import catchAsync from "utils/CatchAsync";
+import {changeListItemPosition} from "utils/ChangeListItemPosition";
+import {useChangePositionQuery} from "hooks/useDetailsData";
 import "./projects.scss";
+import EmptySign from "components/UI/EmptySign";
+import SkeletonLoader from "components/UI/SkeletonLoader";
 
 const Projects = () => {
-    const dispatch = useDispatch();
     const queryClient = useQueryClient();
-    const [viewMode, setViewMode] = useState('table');
     const inboxProject = queryClient.getQueryData('inbox-project');
     const projects = queryClient.getQueryData(['projects', inboxProject?.id]);
+    const {mutateAsync: changePosition} = useChangePositionQuery(['projects', inboxProject?.id]);
+
+    // const onSortEnd = ({oldIndex, newIndex}) => {
+    //     const returnedItems = arrayMoveImmutable(projects, oldIndex, newIndex);
+    //     dispatch(updateProjectPosition(projects[oldIndex].id, newIndex + 1));
+    //     dispatch(setProjects(returnedItems));
+    // };
 
     const onSortEnd = ({oldIndex, newIndex}) => {
-        const returnedItems = arrayMoveImmutable(projects, oldIndex, newIndex);
-        dispatch(updateProjectPosition(projects[oldIndex].id, newIndex + 1));
-        dispatch(setProjects(returnedItems));
-    };
+        catchAsync(async () => {
+            const {list, newPosition} = changeListItemPosition(oldIndex, newIndex, projects);
+            console.log(projects, list, newPosition)
+            const itemId = projects[oldIndex].id;
+            await changePosition({
+                id: itemId,
+                newPosition,
+                type: "project",
+                list
+            });
+        })();
+    }
 
     const renderProjects = () => {
         return (
 
-                <PageContainer widthPadding>
-                    <div className="projects col-12">
-                        <LoadingWrapper show={!!projects} type="circle" size="lg">
-                            {projects && (
+            <PageContainer widthPadding>
+                <div className="projects col-12">
+                    <LoadingWrapper show={!!projects} customLoadingComponent={
+                        <SkeletonLoader
+                            type={"projects"}
+                            width={1250}
+                            height={250}
+                            viewBox="0 0 1250 250"
+                        />
+                    }>
+                        {projects && (
+                            projects.length !== 0 ? (
                                 <ShowProjects
                                     onSortEnd={onSortEnd}
                                     projects={projects}
                                     useDragHandle
                                     axis="xy"
+                                    sortable
                                 />
-                            )}
-                        </LoadingWrapper>
-                        <FloatButton float="r" iconClass="far fa-plus" onClick={() => alert('New Project')}/>
-                    </div>
-                </PageContainer>
+                            ) : (
+                                <EmptySign text="Yoy doesn't have any project"/>
+                            )
+                        )}
+                    </LoadingWrapper>
+                    <FloatButton float="r" iconClass="far fa-plus" onClick={() => alert('New Project')}/>
+                </div>
+            </PageContainer>
 
         );
     };
--- src/pages/Tasks/index.jsx
+++ src/pages/Tasks/index.jsx
@@ -1,49 +1,21 @@
 import React, {useState} from "react";
-import {useDispatch} from "react-redux";
-import {Outlet, useLocation, useNavigate} from "react-router-dom";
+import {Outlet} from "react-router-dom";
 import {useCreateSectionQuery, useSectionsQuery} from "hooks/useSectionsData";
-import catchAsync from "Utils/CatchAsync";
-import {changePosition} from "store/actions/Main.actions";
+import catchAsync from "utils/CatchAsync";
 import PageContainer from "components/UI/PageContainer";
 import LoadingWrapper from "components/UI/LoadingWrapper";
 import SkeletonLoader from "components/UI/SkeletonLoader";
 import CreateInput from "components/CreateInput";
 import ShowSections from "components/ShowSections";
+import {changeListItemPosition} from "utils/ChangeListItemPosition";
+import {useChangePositionQuery} from "hooks/useDetailsData";
 import "./tasks.scss";
 
-const Tasks = ({project, subProject}) => {
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
-    const {pathname} = useLocation();
-    const {data: sections, isLoading} = useSectionsQuery(project?.id, {enabled: !!project});
+const Tasks = ({project}) => {
+    const {data: sections} = useSectionsQuery(project?.id, {enabled: !!project});
     const {mutateAsync: createSection} = useCreateSectionQuery(project?.id);
+    const {mutateAsync: changePosition} = useChangePositionQuery(['project-sections', project.id]);
     const [isCreateButtonDisabled, setIsCreateButtonDisabled] = useState(false);
-
-    // useEffect(() => {
-    //     const fn = async () => {
-    //         try {
-    //             await dispatch(checkUser());
-    //
-    //             if (projectInbox && projectInbox.sections === null) {
-    //                 const result = await dispatch(fetchSections({project: projectInbox.id}));
-    //
-    //                 dispatch(setData({
-    //                     modify: {
-    //                         type: 'project',
-    //                         part: 'projects',
-    //                         id: projectInbox.id,
-    //                         key: 'id',
-    //                         data: {sections: result},
-    //                         nestedProperties: ['projects']
-    //                     }
-    //                 }));
-    //             }
-    //         } catch (error) {
-    //             setServerError(true)
-    //         }
-    //     }
-    //     fn();
-    // }, []);
 
     const createSectionHandler = (title) => {
         catchAsync(async () => {
@@ -58,7 +30,17 @@ const Tasks = ({project, subProject}) => {
     }
 
     const onSortEnd = ({oldIndex, newIndex}) => {
-        dispatch(changePosition('project', 'section', 'sections', project?.id, oldIndex, newIndex, project?.sections));
+        catchAsync(async () => {
+            const {list, newPosition} = changeListItemPosition(oldIndex, newIndex, sections);
+            console.log(sections, list, newPosition)
+            const itemId = sections[oldIndex].id;
+            await changePosition({
+                id: itemId,
+                newPosition,
+                type: "section",
+                list
+            });
+        })();
     }
 
     return (
@@ -84,7 +66,9 @@ const Tasks = ({project, subProject}) => {
                             placeholder="Section name"
                             iconClass="far fa-plus-circle"
                         />
-                        {!!sections && <ShowSections onSortEnd={onSortEnd} useDragHandle axis="x" sections={sections}/>}
+                        {!!sections && (
+                            <ShowSections onSortEnd={onSortEnd} useDragHandle axis="x" sections={sections}/>
+                        )}
                     </div>
                 </LoadingWrapper>
             </PageContainer>
