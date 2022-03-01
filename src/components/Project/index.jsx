14db843cb88089fc77069cb6e31fe1058d306f58
X-Dark-Coder <marzban.dev@gmail.com>
2022-03-01 15:02:20 +0330

    Update project card

--- src/components/Project/index.jsx
+++ src/components/Project/index.jsx
@@ -17,48 +17,40 @@ export const Project = ({project: {project, background, color}, dragHandlerIcon 
 
     return (
         <section
-            className="project"
             onClick={() => navigate(`/project/${project.id}/${project.project}`)}
             style={color ? {backgroundColor: `var(--color-${color})`} : null}
+            className={["project", color ? `project-bg-color-${color}` : null].join(' ')}
         >
             <div className="project-head">
-                {background && <ProfileWallpaper
-                    wallpaperPicture={background}
-                    className="project-head-background"
-                    darkLayerOpacity={0.4}
-                />}
+                {background && (
+                    <ProfileWallpaper
+                        wallpaperPicture={background}
+                        className="project-head-background"
+                        darkLayerOpacity={0.4}
+                    />
+                )}
                 <div className="project-head-top">
                     <div className="project-head-top-info">
                         {dragHandlerIcon && <DragHandle/>}
                         <div className="project-head-top-sections-count">
-                            <div>
-                                <span>67</span>
-                            </div>
-                            sections
+                            <div><span>67</span></div>sections
                         </div>
                         <div className="project-head-top-tasks-count">
-                            <div>
-                                <span>67</span>
-                            </div>
-                            tasks
+                            <div><span>67</span></div>tasks
                         </div>
                     </div>
                 </div>
                 <div className="project-head-bottom">
                     <h3 className="project-head-bottom-title">{project.title}</h3>
                     <p className="project-head-bottom-text">
-                        <span>67</span>
-                        sub-project
+                        <span>67</span>sub-project
                     </p>
                 </div>
             </div>
 
             <div className="project-body">
-                {project.schedule ?
-                    <ScheduleProgressBar createdTime={project.created} deadTime={project.schedule}/> : null}
-                <ScheduleProgressBar createdTime={project.created}
-                                     deadTime={'Sun Feb 06 2022 13:59:24 GMT+0330 (Iran Standard Time)'}/>
-
+                {project.schedule ? <ScheduleProgressBar createdTime={project.created} deadTime={project.schedule}/> : null}
+                <ScheduleProgressBar width={180} createdTime={project.created} deadTime={'Sun Feb 06 2022 13:59:24 GMT+0330 (Iran Standard Time)'}/>
                 <Members compressed limit={3} members={project.users}/>
             </div>
         </section>
