fb7c46bc6fb7ef4f75cadf1efe0e4918e26d8d82
X-Dark-Coder <marzban.dev@gmail.com>
2022-03-01 15:01:00 +0330

    Update member

--- src/components/Member/index.jsx
+++ src/components/Member/index.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import PropTypes from "prop-types";
 import "./member.scss";
 
-const Member = ({picture, name, widthSize = "20px", defaultIconSize = "20px", disableHover, style}) => {
+const Member = ({picture, name, defaultIconSize = "20px", disableHover, style}) => {
+
     return (
         <div
             className={["member", !disableHover ? "member-hover-effect" : null].join(' ')}
             data-tip={name}
-            style={{width: widthSize, ...style}}
+            data-effect="solid"
+            style={style}
         >
             {picture ? <img src={picture} alt={name}/> :
                 <span style={{fontSize: defaultIconSize}} className="far fa-user-circle"></span>}
@@ -19,7 +21,6 @@ Member.propTypes = {
     picture: PropTypes.string.isRequired,
     name: PropTypes.string.isRequired,
     style: PropTypes.object,
-    widthSize: PropTypes.string,
     defaultIconSize: PropTypes.string,
     disableHover: PropTypes.bool,
 }
