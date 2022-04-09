a3e1ccd161aacf84cfa49aa1121d82447117807f
X-Dark-Coder <marzban.dev@gmail.com>
2022-04-10 00:20:25 +0430

    Update label

--- src/components/Label/index.jsx
+++ src/components/Label/index.jsx
@@ -1,43 +1,26 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import catchAsync from "utils/CatchAsync";
-import {useDeleteLabelQuery} from "hooks/useDetailsData";
-import SelectMenu from "components/UI/SelectMenu";
+import { useDeleteLabelQuery } from "hooks/useDetailsData";
 import "./label.scss";
 
-const Label = ({id, title, active, onLabelSelected}) => {
+const Label = ({ id, title, active, onLabelSelected }) => {
+    const { mutateAsync: deleteLabel } = useDeleteLabelQuery();
 
-    const {mutateAsync: deleteLabel, isLoading} = useDeleteLabelQuery();
-
-    const deleteLabelHandler = catchAsync(async () => {
-        await deleteLabel(id);
-    }, {
-        onLoad: "Deleting label",
-        onSuccess: "Label deleted",
-        onError: "Delete label failed"
-    });
+    const deleteLabelHandler = catchAsync(
+        async () => {
+            await deleteLabel(id);
+        },
+        {
+            onLoad: "Deleting label",
+            onSuccess: "Label deleted",
+            onError: "Delete label failed",
+        }
+    );
 
     const [isMouseInLabel, setIsMouseInLabel] = useState(false);
 
-    const selectMenuOptions = [
-        {
-            text: "Edit",
-            iconClass: "far fa-pen",
-            action: () => {
-            },
-        },
-        {
-            text: "Delete",
-            iconClass: "far fa-trash-alt",
-            action: deleteLabelHandler,
-            disabled: isLoading,
-            yesNoQAlert: true,
-            color: "danger"
-        }
-    ]
-
     return (
         <React.Fragment>
-
             <input
                 type="radio"
                 id={`label-${id}`}
@@ -51,24 +34,23 @@ const Label = ({id, title, active, onLabelSelected}) => {
                 className={[
                     "label",
                     active ? "label-active" : null,
-                    !active ? (isMouseInLabel ? "label-hover" : null ) : null
+                    !active ? (isMouseInLabel ? "label-hover" : null) : null,
                 ].join(" ")}
                 htmlFor={`label-${id}`}
                 onMouseEnter={(e) => setIsMouseInLabel(true)}
                 onMouseLeave={(e) => setIsMouseInLabel(false)}
             >
-                <div className="label-border"></div>
+                <div className="label-border" />
                 <div className="label-text-wrapper">
                     <span className="label-text">{title}</span>
-                    <SelectMenu options={selectMenuOptions} type="executable-options" buttonAxis="v"/>
+                    <span className="far fa-trash-alt" onClick={deleteLabelHandler} />
                 </div>
 
                 <div className="label-shape">
-                    <div></div>
+                    <div />
                 </div>
             </label>
-
         </React.Fragment>
-    )
-}
+    );
+};
 export default Label;
